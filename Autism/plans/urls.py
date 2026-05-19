ea37c3cef8310751e26dbbf1ce0022bfa68c2dde
Norah Saeed <norahseedw@gmail.com>
2026-05-19 20:15:38 +0300

    change desgin plan page

--- Autism/plans/urls.py
+++ Autism/plans/urls.py
@@ -5,5 +5,8 @@ app_name="plans"
 
 urlpatterns=[
     path('main_plan/', views.main_plan_view, name='main_plan_view'),
-    path('support_plan/', views.support_plan_view, name='support_plan_view')
+    path('support/plan/', views.support_plan_view, name='support_plan_view'),
+    path('video/plan/', views.video_plan_view, name='video_plan_view'),
+    path('support/strategies/plan/', views.support_strategies_view, name='support_strategies_view')
+
 ]
